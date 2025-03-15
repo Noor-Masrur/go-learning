dd16d44a66182ec83b08cb70bcbbce1e93026a78
noor-masrur <noor.masrur.bd@gmail.com>
2025-03-15 15:57:10 +0600

    channels part -2

--- go/channels/main.go
+++ go/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,20 +12,33 @@ func main() {
 		"https://www.facebook.com",
 		"https://www.amazon.com",
 		"https://www.golang.org",
-		"https://www.roseheaven.io",
+		"https://www.microsoft.com",
 	}
 
+	c := make(chan string)
+
+	start := time.Now()
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+
 	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+	// fmt.Println(<-c)
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println("Time elapsed: ", elapsed.Milliseconds(), "ms")
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
 		return
 	}
-
+	c <- link
 	fmt.Println(link, "is up!")
 }
